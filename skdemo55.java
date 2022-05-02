2087010524d5aefebf87036336614104cced8e8f
jrbryant55 <jrbryant@ucsd.edu>
2022-05-01 23:40:01 -0700

    cocooc

--- skdemo55.java
+++ skdemo55.java
@@ -1,4 +1,6 @@
 public class skdemo55{
 
-
+    public static int multiply(int x, int y){
+        return x*y;
+    }
 }
---
+++ skdemo55tests.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class skdemo55tests{
+    @Test
+    public void multiply(){
+        assertEquals(7, skdemo55.multiply(2,3));
+    }
+}
